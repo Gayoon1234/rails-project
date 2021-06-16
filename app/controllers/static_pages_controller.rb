6e84f8b4f62c635eaefd0a0bad33a1e7bcf4b2c0
Gayoon1234 <s3843584@student.rmit.edu.au>
2021-06-16 23:21:21 +0000

    added quizapi

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
+    @out = `curl https://quizapi.io/api/v1/questions -G -d apiKey=QDPJY0cQLHwllKvmAmygqEm5iOxEjlyLhoeUxViy -d limit=10`
   end
 end
