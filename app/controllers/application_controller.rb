2861a3aaa9b95615254dfb90f5d0e0f58203ab0b
Gayoon1234 <s3843584@student.rmit.edu.au>
2021-06-17 07:18:14 +0000

    api integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require 'json'
 class ApplicationController < ActionController::Base
-  file = File.read("quiz.json")
+  file = `curl https://quizapi.io/api/v1/questions -G -d apiKey=QDPJY0cQLHwllKvmAmygqEm5iOxEjlyLhoeUxViy -d limit=10`
+  file ||= File.read("quiz.json")
   @@json_hash = JSON.parse(file)
 end
